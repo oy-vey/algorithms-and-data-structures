81beb93dc52e1e3d97766c8bd5a03c1a4425cbb5
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2017-07-13 18:49:20 +0300

    fibonacci.py

--- 1-AlgorithmicToolbox/Week2/fibonacci/fibonacci.py
+++ 1-AlgorithmicToolbox/Week2/fibonacci/fibonacci.py
@@ -1,9 +1,12 @@
 # Uses python3
 def calc_fib(n):
-    if (n <= 1):
-        return n
+    fibs = [None] * max(n + 1, 2)
+    fibs[0] = 0
+    fibs[1] = 1
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    for i in range(2, n + 1):
+        fibs[i] = fibs[i-1] + fibs[i-2]
+    return fibs[n]
 
 n = int(input())
 print(calc_fib(n))
