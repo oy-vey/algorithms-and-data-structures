7cc90bde1254cf8fcdf125933a8508ae776880f0
Daniil Skokleenko <dcman1993@icloud.com>
2017-07-29 00:44:16 +0300

    majority_element

--- 1-AlgorithmicToolbox/Week4/majority_element/majority_element.py
+++ 1-AlgorithmicToolbox/Week4/majority_element/majority_element.py
@@ -8,9 +8,17 @@ def get_majority_element(a, left, right):
         return a[left]
     maxl = get_majority_element(a, left, (left + right) // 2)
     maxr = get_majority_element(a, (left + right) // 2,  right)
-    if maxl == maxr:
-        return [maxl]
-    #if n == len(a) and ()
+    l = []
+    if maxl >= 0: l.append(maxl)
+    if maxr >= 0: l.append(maxr)
+    for item in l:
+        CurrentElement = item
+        count = 0
+        for j in range(left, right):
+            if a[j] == CurrentElement:
+                count += 1
+                if count > (right - left) // 2:
+                    return item
     return -1
 
 if __name__ == '__main__':
