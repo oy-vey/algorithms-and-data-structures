82f54cdc8d019868cc4760c2a1e9925aa067fd85
Daniil Skokleenko <dcman1993@icloud.com>
2018-01-09 02:39:35 +0300

    draft .sh test file added

---
+++ 2-DataStructures/Week1/check_brackets_in_code/Test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FILES=./tests/*
+
+clear
+for f in $FILES
+do
+
+  if [[ $f == *"a"* ]]
+  then
+    echo "Right answer:"
+    cat $f
+  else
+    echo "Processing $f file..."
+    echo "Result:"
+    cat $f | python check_brackets.py
+  fi
+
+done
