5ff76e24e2be7a5da9cab59e85d0d9923f81226b
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2018-01-08 18:26:59 +0300

    Test PS script

---
+++ 2-DataStructures/Week1/check_brackets_in_code/Test.ps1
@@ -0,0 +1,13 @@
+﻿clear
+$tests = Get-ChildItem -Path ./tests -Exclude *.a 
+$answers = Get-ChildItem -Path ./tests/*.a 
+for ($i=0; $i -lt $tests.Count; $i++) {
+    echo "_____________________"
+    echo $tests[$i].FullName
+    echo "Result: "
+    type $tests[$i].FullName | python .\check_brackets.py
+    echo "Correct Answer: "
+    type $answers[$i].FullName
+    echo "_____________________"
+
+}
