d53d2458c641c11e99ede20909268aef53967d5d
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2018-01-11 13:10:46 +0300

    powershell file for testing updated

--- 2-DataStructures/Week1/check_brackets_in_code/Test.ps1
+++ 2-DataStructures/Week1/check_brackets_in_code/Test.ps1
@@ -2,12 +2,18 @@
 $tests = Get-ChildItem -Path ./tests -Exclude *.a 
 $answers = Get-ChildItem -Path ./tests/*.a 
 for ($i=0; $i -lt $tests.Count; $i++) {
-    echo "_____________________"
-    echo $tests[$i].FullName
-    echo "Result: "
-    type $tests[$i].FullName | python .\check_brackets.py
-    echo "Correct Answer: "
-    type $answers[$i].FullName
-    echo "_____________________"
+    echo "Test#" $tests[$i].Name
+    $Result = type $tests[$i].FullName | python .\check_brackets.py
+    $Answer = type $answers[$i].FullName
+    
+    If ($Result -ne $Answer) {
+        echo "Failed" $tests[$i].Name
+        echo "Result:" $Result
+        echo "Correct answer:" $Answer
+    }
+    else{
+        echo "Success"
+    }
+    echo "____________________"
 
 }
