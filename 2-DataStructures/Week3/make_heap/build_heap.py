12ed7743b83ad781048b3bfe49ddea0ac832079d
DaniilSkokleenko <Daniil_Skokleenko@epam.com>
2018-03-21 15:31:58 +0300

    build_heap.py test passed

--- 2-DataStructures/Week3/make_heap/build_heap.py
+++ 2-DataStructures/Week3/make_heap/build_heap.py
@@ -1,38 +1,57 @@
 # python3
 
 class HeapBuilder:
-  def __init__(self):
-    self._swaps = []
-    self._data = []
+    def __init__(self):
+        self._swaps = []
+        self._data = []
 
-  def ReadData(self):
-    n = int(input())
-    self._data = [int(s) for s in input().split()]
-    assert n == len(self._data)
+    def ReadData(self):
+        n = int(input())
+        self._data = [int(s) for s in input().split()]
+        assert n == len(self._data)
 
-  def WriteResponse(self):
-    print(len(self._swaps))
-    for swap in self._swaps:
-      print(swap[0], swap[1])
+    def WriteResponse(self):
+        print(len(self._swaps))
+        for swap in self._swaps:
+            print(swap[0], swap[1])
 
-  def GenerateSwaps(self):
-    # The following naive implementation just sorts 
+    def SiftDown(self, i):
+        size = len(self._data)
+        minIndex = i
+        l = 2*i + 1
+        if l < size and self._data[l] < self._data[minIndex]:
+            minIndex = l
+        r = 2*i + 2
+        if r < size and self._data[r] < self._data[minIndex]:
+            minIndex = r
+        if i != minIndex:
+            self._swaps.append((i, minIndex))
+            self._data[i], self._data[minIndex] = self._data[minIndex], self._data[i]
+            self.SiftDown(minIndex)
+
+    def GenerateSwaps(self):
+
+    # The following naive implementation just sorts
     # the given sequence using selection sort algorithm
     # and saves the resulting sequence of swaps.
-    # This turns the given array into a heap, 
+    # This turns the given array into a heap,
     # but in the worst case gives a quadratic number of swaps.
     #
-    # TODO: replace by a more efficient implementation
-    for i in range(len(self._data)):
-      for j in range(i + 1, len(self._data)):
-        if self._data[i] > self._data[j]:
-          self._swaps.append((i, j))
-          self._data[i], self._data[j] = self._data[j], self._data[i]
+    #     for i in range(len(self._data)):
+    #       for j in range(i + 1, len(self._data)):
+    #         if self._data[i] > self._data[j]:
+    #           self._swaps.append((i, j))
+    #           self._data[i], self._data[j] = self._data[j], self._data[i]
 
-  def Solve(self):
-    self.ReadData()
-    self.GenerateSwaps()
-    self.WriteResponse()
+        size = len(self._data)
+        for i in range((size // 2) - 1, -1, -1):
+            self.SiftDown(i)
+
+
+    def Solve(self):
+        self.ReadData()
+        self.GenerateSwaps()
+        self.WriteResponse()
 
 if __name__ == '__main__':
     heap_builder = HeapBuilder()
